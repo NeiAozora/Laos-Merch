59e8dde82008ea01cb9c2a5e109480c6035a0ec5
ChemicalSpark <rangga.pramudya23@gmail.com>
2024-07-28 19:55:42 +0700

    menambah cartitemmodel dan cartcontroller (belum selesai)

--- app/controllers/cartController.php
+++ app/controllers/cartController.php
@@ -1,6 +1,12 @@
 <?php
 
 class CartController extends Controller{
+    private $cartItemModel;
+
+    public function __construct() {
+        $this->cartItemModel = new CartItemModel();
+    }
+
     public function index(){
         $this->view('cart/index');
     }
--- app/models/OrderModel.php
+++ app/models/OrderModel.php
@@ -1,7 +1,25 @@
 <?php
 
 class OrderModel extends Model {
+    protected $db;
     protected $table = 'orders';
     protected $primaryKey = 'id_order';
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    //add
+    
+
+    //get
+
+
+    //update
+
+
+    //delete    
+
+    
 }
 
