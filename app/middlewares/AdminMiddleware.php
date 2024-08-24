a79e9206a1a5bf423009548dd8b3fbdd29d501af
degeneratezan <noxindocraft@gmail.com>
2024-08-24 13:37:21 +0700

    fixed auth to admin

--- app/middlewares/AdminMiddleware.php
+++ app/middlewares/AdminMiddleware.php
@@ -4,15 +4,17 @@ class AdminMiddleware extends Middleware
 {
     public function checkLoginSession()
     {
+        if (!isset($_SESSION["user"]['fr'])){
+            jsRedirect(BASEURL . "login?message=" . urlencode("Maaf anda diharuskan untuk login!"));
+        }
 
-        $accessToken = $_SESSION["fr"];
-        $uid = $_SESSION["br"];
-        $result = AuthHelpers::verifyFBAccessIdToken($accessToken);
+        $accessToken = $_SESSION['user']["fr"];
+        $result = AuthHelpers::getLoggedInUserData($accessToken);
         if (!$result){
             header('Location:' . BASEURL . 'login?message=' . urlencode("Maaf anda diharuskan untuk login!"));
             exit();
         }
-
+        
         if ($result['role_name'] != 'Admin'){
             header('Location:' . BASEURL . 'login?message=' . urlencode("Anda tidak memiliki hak akses!"));
             exit();
--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -154,10 +154,12 @@ function encodeBase64(data) {
                             </ul>
                         </div>
                         <?php if(AuthHelpers::getLoggedInUserData()["role_name"] == "Admin"): ?>
-                            <button class="btn btn-warning active me-2 dropdown-toggle">
-                                <i class="fa-solid fa-key me-2"></i>
-                                <a style="text-decoration: none; color: inherit" href="<?= BASEURL ?>admin">Admin</a>
-
+                            <a style="text-decoration: none; color: inherit" href="<?= BASEURL ?>admin">
+                                <button class="btn btn-warning active me-2 dropdown-toggle">
+                                    <i class="fa-solid fa-key me-2"></i>
+                                    Admin
+                                </button>
+                            </a>
                         <?php endif; ?>
                     <?php else: ?>
                         <button class="btn btn-success"><a style="text-decoration: none; color: inherit" href="<?= BASEURL ?>login">Login</a></button>
